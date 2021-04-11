9f46cf31694f5d52b0cb87dfcf604e7e84c3c18d
mateushonor <48721409+mateushonor@users.noreply.github.com>
2021-04-10 22:58:36 -0300

    Add files via upload

---
+++ Membro.java
@@ -0,0 +1,33 @@
+public class Membro {
+	private String nome;
+	private int idade;
+	private int tempo;
+	private String equipe;
+	
+	public void setNome(String _nome) {
+		nome = _nome;
+	}
+	public void setIdade(int _idade) {
+		idade = _idade;
+	}
+	public void setTempo(int _tempo) {
+		tempo = _tempo;
+	}
+	public void setEquipe(String _equipe) {
+		equipe = _equipe;
+	}
+	
+	public String getNome() {
+		return(nome);
+	}
+	public int getIdade() {
+		return(idade);
+	}
+	public int getTempo() {
+		return(tempo);
+	}
+	public String getEquipe() {
+		return(equipe);
+	}
+
+}
